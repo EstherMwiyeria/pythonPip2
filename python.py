71cf9d1e9cc3c05ce01b2f974ca9caa9fa35d2c8
EstherMwiyeria <esthermkanoro@gmail.com>
2023-05-06 00:35:43 +0300

    new

--- python.py
+++ python.py
@@ -23,8 +23,17 @@ print(list_of_words(["MaryAnne","JaneClever","Blige"]))
 
 # 3. Write a Python program that accepts a comma-separated sequence of words 
 # as input and prints the distinct words in sorted form (alphanumerically).
-def arrange_words(words):
-    list_words = words.split(",")
+def sort_distinct_words(words):
+    words = words.split(",")
+    
+    words = [word.strip() for word in words]
+   
+    distinct_words = sorted(set(words))
+
+    return distinct_words
+print(sort_distinct_words("kelly,nelly,bekky,lely,shelly,velly"))
+
+
 
 # 4.. Write a Python function that takes two lists and returns True if they have
 #  at least one common member.
@@ -40,6 +49,14 @@ print(common_member([1,2,3,4,5], [5,6,7,8,9]))
 
 # 5. Write a Python program to convert a list to a list of dictionaries.
 # Sample lists: ["Black", "Red", "Maroon", "Yellow"], ["#000000", "#FF0000", "#800000", "#FFFF00"]
+def list_to_dict(keys, values):
+    dict_list = [{keys[i]: values[i]} for i in range(len(keys))]
+    return dict_list
+print(list_to_dict(["Black", "Red", "Maroon", "Yellow"], ["#000000", "#FF0000", "#800000", "#FFFF00"]))
+
+result = [num for num in range(1, 1001) if any(num % d == 0 for d in range(2, 10))]
+print(result)
+
 
 # 6. Write a Python program to check whether all dictionaries in a list are empty or not
 my_list = [{},{},{}]
@@ -49,7 +66,7 @@ print(all(not d for d in my_list))
 # numbers = [3,5,45,97,32,22,10,19,39,43]
 numbers = [3,5,45,97,32,22,10,19,39,43]
 even_numbers = [n for n in numbers if n%2 == 0 ]
-print(even_numbers([3,5,45,97,32,22,10,19,39,43]))
+print(even_numbers)
 
 # 8. Find the common numbers in two lists (without using a tuple or set) 
 # list_a = 1, 2, 3, 4, 
@@ -60,7 +77,12 @@ list_b = [2,3,4,5]
 common_numbers = [x for x in list_a if x in list_b]
 print (common_numbers)
 
-# 9. Use a nested list comprehension to find all of the numbers from 1-1000 that are divisible by any single digit besides 1 (2-9)
+# 9. Use a nested list comprehension to find all of the numbers from 1-1000 that
+#  are divisible by any single digit besides 1 (2-9)
+total_numbers=list(range(1,1000))
+total_nums=list(range(2,9))
+y = [number for number in range(1,1000) if True in [True for x in range(2,9) if number % x == 0]]
+print(y)
 
 # 10. Write a Python function to remove all vowels in a string
 def remove_vowels(country):
